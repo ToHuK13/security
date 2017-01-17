b496626f336b53c1654394df14681f8281e1fc93
ToHuK13 <tohuk13@yandex.ru>
2017-01-17 23:31:19 +0300

    форма авторизации

--- security/security/Form1.cs
+++ security/security/Form1.cs
@@ -7,20 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace security
 {
-    public partial class Form1 : Form
+    public partial class AutorizeForm : Form
     {
-        public Form1()
+        public AutorizeForm()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hellow world");
 
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source='" + textBox1.Text + "';Password=078013;User ID=sa;Initial Catalog=LIG;Persist Security Info=True;"); //Integrated Security=True;");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Users where UserName = '" + textBox2.Text + "' and Password = '" + maskedTextBox1.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Main ss = new Main();
+                ss.Show();
+            }
+            else {
+                MessageBox.Show("check username or password");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- security/security/Program.cs
+++ security/security/Program.cs
@@ -16,7 +16,7 @@ namespace security
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AutorizeForm());
         }
     }
 }
